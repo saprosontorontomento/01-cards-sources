69e08d487d9541815e47bda9b9681763ec7887b8
econom1st <camcycamovu4@gmail.com>
2021-06-12 09:27:16 +0300

    cards sources

---
+++ app.js
@@ -0,0 +1,21 @@
+function slidesPlugin(activeSlide = 3) {
+    const slides = document.querySelectorAll('.slide');
+
+    slides[activeSlide].classList.add('active');
+    
+    slides.forEach((slide) => {
+        slide.addEventListener('click', () => {
+            clearActiveClasses()
+    
+            slide.classList.add('active')
+        })
+    })
+    
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+}
+
+slidesPlugin()
